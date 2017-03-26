6bceb61ac0b0611d6c022bb8f00d020f3baf4b94
Sven Walter <s.walter@rebuy.com>
2017-03-26 18:44:10 +0200

    fixup! test

--- vars/githubLatest.groovy
+++ vars/githubLatest.groovy
@@ -1,9 +1,11 @@
 def call(repo) {
-	return sh(
-		script: """curl -s \
-			-H "Accept: application/vnd.github.v3+json" \
-			"https://api.github.com/repos/${repo}/releases/latest" \
-			| jq '.tag_name' -r""",
-		returnStdout: true
-	).trim()
+	stage('GitHub') {
+		return sh(
+			script: """curl -s \
+				-H "Accept: application/vnd.github.v3+json" \
+				"https://api.github.com/repos/${repo}/releases/latest" \
+				| jq '.tag_name' -r""",
+			returnStdout: true
+		).trim()
+	}
 }
